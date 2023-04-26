73a46906c9e0f6fd5633365515da58bfe33a8dde
CharlesLmz <charles569716@gmail.com>
2023-04-26 16:34:41 +0800

    完善对比图绘制

--- compare_experiment_SL.py
+++ compare_experiment_SL.py
@@ -14,8 +14,37 @@ env = CGEnv(args)
 
 net = Net.GAT(node_feature_dim=6, column_feature_dim=3, embed_dim=256, device=args.device)
 actor = Net.Actor(net)
-actor.load_state_dict(torch.load("pretrain\\model_saved\\actor.pth", map_location=torch.device('cpu')))
+actor.load_state_dict(torch.load("pretrain\\model_saved\\actor_standard.pth", map_location=torch.device('cpu')))
 start_time = time.time()
+
+# original column generation
+state, info = env.reset(args.instance)
+iter_cnt = 0
+reward_list = []
+origin_ub_lb = []
+vehicleNum = 50 if env.CGAlg.graph.nodeNum < 400 else 100 # set vehicleNum manually
+while True:
+    # test: randomly delete a column
+    col_num = len(state["columns_state"])
+    action = np.ones(col_num)
+    probs = actor(state, info)
+    # for i in range(len(action)):
+    #     if np.random.rand() < probs[i][0]:
+    #         action[i] = 0
+    state, reward, done, info = env.step(action)
+    RMP = env.CGAlg.solve_final_RMP()
+    ub = RMP.ObjVal
+    lb = max(RMP.ObjVal + min(vehicleNum * env.CGAlg.SP_obj, 0), 0)
+    reward_list.append(reward)
+    origin_ub_lb.append([ub, lb])
+    print("Iter {}: delete column {}, reward = {}".format(iter_cnt, len(action)-sum(action), reward))
+    if done:
+        break
+    iter_cnt += 1
+time_cost = time.time() - start_time
+print("origin: final_obj = {}, iter_cnt = {}, total_reward = {}, time_cost = {}".format(env.CGAlg.RLMP_obj, iter_cnt, sum(reward_list), time_cost))
+
+# model column generation
 state, info = env.reset(args.instance)
 iter_cnt = 0
 reward_list = []
@@ -27,11 +56,12 @@ while True:
     action = np.ones(col_num)
     probs = actor(state, info)
     for i in range(len(action)):
-        if np.random.rand() < probs[i][1]:
+        if np.random.rand() < probs[i][0]:
             action[i] = 0
     state, reward, done, info = env.step(action)
-    ub = env.CGAlg.RLMP_obj
-    lb = max(env.CGAlg.RLMP_obj - min(vehicleNum * env.CGAlg.SP_obj, 0), 0)
+    RMP = env.CGAlg.solve_final_RMP()
+    ub = RMP.ObjVal
+    lb = max(RMP.ObjVal + min(vehicleNum * env.CGAlg.SP_obj, 0), 0)
     reward_list.append(reward)
     model_ub_lb.append([ub, lb])
     print("Iter {}: delete column {}, reward = {}".format(iter_cnt, len(action)-sum(action), reward))
@@ -39,6 +69,11 @@ while True:
         break
     iter_cnt += 1
 time_cost = time.time() - start_time
-print("final_obj = {}, iter_cnt = {}, total_reward = {}, time_cost = {}".format(env.CGAlg.RLMP_obj, iter_cnt, sum(reward_list), time_cost))
-plt.plot(range(len(model_ub_lb)), np.array(model_ub_lb))
+print("model: final_obj = {}, iter_cnt = {}, total_reward = {}, time_cost = {}".format(env.CGAlg.RLMP_obj, iter_cnt, sum(reward_list), time_cost))
+
+plt.plot(range(len(origin_ub_lb)), np.array(origin_ub_lb)[:,0], label="origin_ub")
+plt.plot(range(len(origin_ub_lb)), np.array(origin_ub_lb)[:,1], label="origin_lb")
+plt.plot(range(len(model_ub_lb)), np.array(model_ub_lb)[:,0], label="model_ub")
+plt.plot(range(len(model_ub_lb)), np.array(model_ub_lb)[:,1], label="model_lb")
+plt.legend()
 plt.show()
