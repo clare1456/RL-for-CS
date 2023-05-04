1770f37ef7e8ff989d4903309f69e0cb5f692c0f
CharlesLee <594212038@qq.com>
2023-05-04 18:08:59 +0800

    更新测试数据的保存及绘图

--- compare_experiment_SL.py
+++ compare_experiment_SL.py
@@ -9,11 +9,11 @@ from pretrain.dataProcess import MILPSolver
 from run import Args
 import Net
 import torch
-import pickle
+import json
 import os
 
 args = Args()
-args.instancce = "C1_2_2"
+args.instance = "C1_2_1"
 args.max_step = 200
 
 net = Net.GAT4(node_feature_dim=6, column_feature_dim=3, embed_dim=256, device=args.device)
@@ -76,7 +76,8 @@ def column_generation(model = "origin"):
         ub_lb_list.append([ub, lb])
         RLMP_time_list.append(env.CGAlg.RLMP_timeRecord)
         time_list.append(time.time() - start_time)
-        print("Iter {}: delete column {}, reward = {}, extra_route_num = {}".format(iter_cnt, len(action)-sum(action), reward, info["extra_route_num"]))
+        print("Iter {}: delete column {}, extra_route_num = {}, reward = {}, column_num = {}"
+              .format(iter_cnt, len(action)-sum(action), reward, info["extra_route_num"], len(env.CGAlg.column_pool)))
         if done:
             break
         iter_cnt += 1
@@ -87,54 +88,87 @@ def column_generation(model = "origin"):
         print("model: final_obj = {}, iter_cnt = {}, total_reward = {}, time_cost = {}".format(env.CGAlg.RLMP_obj, iter_cnt, sum(reward_list), time_cost))
     return ub_lb_list, RLMP_time_list, time_list
         
+if not os.path.exists("outputs/data/compare_{}.json".format(args.instance)):
+    # run column generation
+    greedy_ub_lb_list, greedy_RLMP_time_list, greedy_time_list = column_generation("greedy")
+    origin_ub_lb_list, origin_RLMP_time_list, origin_time_list = column_generation("origin")
+    MILP_ub_lb_list, MILP_RLMP_time_list, MILP_time_list = column_generation("MILP")
+    model_ub_lb_list, model_RLMP_time_list, model_time_list = column_generation(actor)
+    # preprocess
+    origin_ub_list = np.array(origin_ub_lb_list)[:,0]
+    origin_lb_list = np.array(origin_ub_lb_list)[:,1]
+    max_origin_ub = max(origin_ub_list)
+    min_origin_ub = min(origin_ub_list)
+    max_origin_lb = max(origin_lb_list)
+    min_origin_lb = min(origin_lb_list)
+    model_ub_list = np.array(model_ub_lb_list)[:,0]
+    model_lb_list = np.array(model_ub_lb_list)[:,1]
+    greedy_ub_list = np.array(greedy_ub_lb_list)[:,0]
+    greedy_lb_list = np.array(greedy_ub_lb_list)[:,1]
+    MILP_ub_list = np.array(MILP_ub_lb_list)[:,0]
+    MILP_lb_list = np.array(MILP_ub_lb_list)[:,1]
+    origin_iter_list = np.arange(len(origin_ub_list))
+    model_iter_list = np.arange(len(model_ub_list))
+    greedy_iter_list = np.arange(len(greedy_ub_list))
+    MILP_iter_list = np.arange(len(MILP_ub_list))
+    # normalize
+    # origin_ub_list = (origin_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
+    # origin_lb_list = (origin_lb_list - min_origin_lb) / (max_origin_lb - min_origin_lb)
+    # model_ub_list = (model_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
+    # model_lb_list = (model_lb_list - min_origin_lb) / (max_origin_lb - min_origin_lb)
+    # greedy_ub_list = (greedy_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
+    # greedy_lb_list = (greedy_lb_list - min_origin_lb) / (max_origin_lb - min_origin_lb)
+    # MILP_ub_list = (MILP_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
+    # MILP_lb_list = (MILP_lb_list - min_origin_lb) / (max_origin_lb - min_origin_lb)
+    # save data
+    data = {
+        "origin_ub_list": list(origin_ub_list),
+        "model_ub_list": list(model_ub_list),
+        "greedy_ub_list": list(greedy_ub_list),
+        "MILP_ub_list": list(MILP_ub_list),
+        "origin_lb_list": list(origin_lb_list),
+        "model_lb_list": list(model_lb_list),
+        "greedy_lb_list": list(greedy_lb_list),
+        "MILP_lb_list": list(MILP_lb_list),
 
-# run column generation
-greedy_ub_lb_list, greedy_RLMP_time_list, greedy_time_list = column_generation("greedy")
-origin_ub_lb_list, origin_RLMP_time_list, origin_time_list = column_generation("origin")
-MILP_ub_lb_list, MILP_RLMP_time_list, MILP_time_list = column_generation("MILP")
-model_ub_lb_list, model_RLMP_time_list, model_time_list = column_generation(actor)
-# preprocess
-origin_ub_list = np.array(origin_ub_lb_list)[:,0]
-max_origin_ub = max(origin_ub_list)
-min_origin_ub = min(origin_ub_list)
-origin_ub_list = (origin_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
-model_ub_list = np.array(model_ub_lb_list)[:,0]
-model_ub_list = (model_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
-greedy_ub_list = np.array(greedy_ub_lb_list)[:,0]
-greedy_ub_list = (greedy_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
-MILP_ub_list = np.array(MILP_ub_lb_list)[:,0]
-MILP_ub_list = (MILP_ub_list - min_origin_ub) / (max_origin_ub - min_origin_ub)
-origin_iter_list = np.arange(len(origin_ub_list))
-model_iter_list = np.arange(len(model_ub_list))
-greedy_iter_list = np.arange(len(greedy_ub_list))
-MILP_iter_list = np.arange(len(MILP_ub_list))
-# save data
-data = {
-    "origin_ub_list": list(origin_ub_list),
-    "model_ub_list": list(model_ub_list),
-    "greedy_ub_list": list(greedy_ub_list),
-    "MILP_ub_list": list(MILP_ub_list),
+        "origin_RLMP_time_list": list(origin_RLMP_time_list),
+        "model_RLMP_time_list": list(model_RLMP_time_list),
+        "greedy_RLMP_time_list": list(greedy_RLMP_time_list),
+        "MILP_RLMP_time_list": list(MILP_RLMP_time_list),
 
-    "origin_RLMP_time_list": list(origin_RLMP_time_list),
-    "model_RLMP_time_list": list(model_RLMP_time_list),
-    "greedy_RLMP_time_list": list(greedy_RLMP_time_list),
-    "MILP_RLMP_time_list": list(MILP_RLMP_time_list),
-
-    "origin_time_list": list(origin_time_list),
-    "model_time_list": list(model_time_list),
-    "greedy_time_list": list(greedy_time_list),
-    "MILP_time_list": list(MILP_time_list),
-
-    "origin_iter_list": list(origin_iter_list),
-    "model_iter_list": list(model_iter_list),
-    "greedy_iter_list": list(greedy_iter_list),
-    "MILP_iter_list": list(MILP_iter_list),
-}
-path = "outputs/data/"
-if not os.path.exists(path):
-    os.makedirs(path)
-with open(path + "compare_{}.pkl".format(args.instance), "wb") as f:
-    pickle.dump(data, f)
+        "origin_time_list": list(origin_time_list),
+        "model_time_list": list(model_time_list),
+        "greedy_time_list": list(greedy_time_list),
+        "MILP_time_list": list(MILP_time_list),
+    }
+    path = "outputs/data/"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(path + "compare_{}.json".format(args.instance), "w") as f:
+        json.dump(data, f)
+else:
+    # read data if already exists
+    data = json.load(open("outputs/data/compare_{}.json".format(args.instance), "r"))
+    origin_ub_list = np.array(data["origin_ub_list"])
+    model_ub_list = np.array(data["model_ub_list"])
+    greedy_ub_list = np.array(data["greedy_ub_list"])
+    MILP_ub_list = np.array(data["MILP_ub_list"])
+    origin_lb_list = np.array(data["origin_lb_list"])
+    model_lb_list = np.array(data["model_lb_list"])
+    greedy_lb_list = np.array(data["greedy_lb_list"])
+    MILP_lb_list = np.array(data["MILP_lb_list"])
+    origin_RLMP_time_list = np.array(data["origin_RLMP_time_list"])
+    model_RLMP_time_list = np.array(data["model_RLMP_time_list"])
+    greedy_RLMP_time_list = np.array(data["greedy_RLMP_time_list"])
+    MILP_RLMP_time_list = np.array(data["MILP_RLMP_time_list"])
+    origin_time_list = np.array(data["origin_time_list"])
+    model_time_list = np.array(data["model_time_list"])
+    greedy_time_list = np.array(data["greedy_time_list"])
+    MILP_time_list = np.array(data["MILP_time_list"])
+    origin_iter_list = np.arange(len(origin_ub_list))
+    model_iter_list = np.arange(len(model_ub_list))
+    greedy_iter_list = np.arange(len(greedy_ub_list))
+    MILP_iter_list = np.arange(len(MILP_ub_list))
 # plot graphs
 ## 1. RMP time
 plt.figure()
@@ -142,11 +176,16 @@ plt.plot(origin_RLMP_time_list, origin_ub_list, label="origin_ub")
 plt.plot(model_RLMP_time_list, model_ub_list, label="model_ub")
 plt.plot(greedy_RLMP_time_list, greedy_ub_list, label="greedy_ub")
 plt.plot(MILP_RLMP_time_list, MILP_ub_list, label="MILP_ub")
+plt.plot(origin_RLMP_time_list, origin_lb_list-1, label="origin_lb")
+plt.plot(model_RLMP_time_list, model_lb_list-1, label="model_lb")
+plt.plot(greedy_RLMP_time_list, greedy_lb_list-1, label="greedy_lb")
+plt.plot(MILP_RLMP_time_list, MILP_lb_list-1, label="MILP_lb")
 title = args.instance + " RMP time"
 plt.title(title)
 plt.xlabel("RMP time")
 plt.ylabel("obj")
 plt.legend()
+plt.xlim(0, max(greedy_RLMP_time_list)) # 横坐标截断
 plt.savefig('outputs/pictures/{}.png'.format(title))
 ## 2. total time
 plt.figure()
